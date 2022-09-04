9d6c5b59e1a762a168e683198faa704c4ca873f8
Efana <efana.usua@wemabank.com>
2022-09-04 16:13:23 +0100

    Added project files, dependences and structure

---
+++ app/src/main/java/com/example/android/data/dto/ArticlesResultDto.kt
@@ -0,0 +1,9 @@
+package com.example.android.data.dto
+
+data class ArticlesResultDto(
+    val uri: String?,
+    val url: String?,
+    val id: Int?,
+    val asset_id: Int?,
+    val source: String?
+)
---
+++ app/src/main/java/com/example/android/presentation/MainVM.kt
@@ -0,0 +1,4 @@
+package com.example.android.presentation
+
+class MainVM {
+}

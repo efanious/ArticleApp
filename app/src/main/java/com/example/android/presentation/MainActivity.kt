5df1dd36ae65275e17463e818f4df74f074a359e
Efana <efana.usua@wemabank.com>
2022-09-04 23:23:01 +0100

    Adding Recycler view and other data fields

--- app/src/main/java/com/example/android/presentation/MainActivity.kt
+++ app/src/main/java/com/example/android/presentation/MainActivity.kt
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.android.R
+import com.example.android.adapters.ArticlesAdapter
 import com.example.android.util.Resource
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -17,12 +20,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val articlesRView: RecyclerView = findViewById(R.id.popular_articles_recycler_view)
+        articlesRView.layoutManager =
+            LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+
+        val popularArticlesAdapter = ArticlesAdapter()
+
         viewModel.getArticles()
 
         viewModel.response.observe(this) { result ->
             when (result) {
                 is Resource.Success -> {
-                    Toast.makeText(this, "num_results: ${result.data?.num_results}", Toast.LENGTH_SHORT).show()
+                    //Toast.makeText(this, "num_results: ${result.data?.num_results}", Toast.LENGTH_SHORT).show()
+                    popularArticlesAdapter.data = result.data?.results!!
+                    articlesRView.adapter = popularArticlesAdapter
                 }
                 is Resource.Error -> {
                     Toast.makeText(this, result.message, Toast.LENGTH_SHORT).show()
